b3ea40fe888bbfa8739f0921e3860a3e4d5bfea6
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2024-08-15 19:54:32 -0300

    add plan aggregate

---
+++ domain/src/main/java/io/github/gabrmsouza/subscription/domain/plan/PlanCommand.java
@@ -0,0 +1,14 @@
+package io.github.gabrmsouza.subscription.domain.plan;
+
+import io.github.gabrmsouza.subscription.domain.money.Money;
+
+public sealed interface PlanCommand {
+    record ChangePlan(String name, String description, Money price, Boolean active) implements PlanCommand {
+    }
+
+    record InactivatePlan() implements PlanCommand {
+    }
+
+    record ActivatePlan() implements PlanCommand {
+    }
+}
